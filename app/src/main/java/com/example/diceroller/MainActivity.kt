40bdca0b84354a078e21d84576a137ee9c0ba757
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2021-12-27 21:58:56 +0100

    Refaktoryzacja kodu i komentarze.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,6 +5,10 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Ta aktywność pozwala użytkownikowi rzucać kostką i wyświetlać rezultat
+ * na ekranie.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,14 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rolledButton: Button = findViewById(R.id.button)
-        rolledButton.setOnClickListener {
-            rollDice()
-        }
+        rolledButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Rzuć kostką i wyświetl rezultat na ekranie.
+     */
     private fun rollDice() {
+        // Tworzenie kostki 6-ściennej i rzucenie nią.
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        // Aktualizacja ekranu o rzut kostką.
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
