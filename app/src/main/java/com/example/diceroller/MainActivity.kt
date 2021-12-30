1ad3b2b48223f64fd45acd7f385aa66189380c97
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2021-12-30 16:53:54 +0100

    Dodana druga kostka.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,25 +16,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rolledButton: Button = findViewById(R.id.button)
-        rolledButton.setOnClickListener { rollDice() }
+        rolledButton.setOnClickListener { rollDices() }
 
-        // Rzuć kostką kiedy aplikacja zostanie uruchomiona
-        rollDice()
+        // Rzuć kośćmi kiedy aplikacja zostanie uruchomiona
+        rollDices()
     }
 
     /**
      * Rzuć kostkami i wyświetl rezultat na ekranie.
      */
-    private fun rollDice() {
-        // Tworzenie kostki 6-ściennej i rzucenie nią
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
+    private fun rollDices() {
+        // Tworzenie kości 6-ściennych i rzucenie nimi
+        val downDice = Dice(6)
+        val downDiceRoll = downDice.roll()
+        val upDice = Dice(6)
+        val upDiceRoll = upDice.roll()
 
         // Znajdź ImagView z pliku wartsw
-        val diceImage: ImageView = findViewById(R.id.imageView)
+        val downDiceImage: ImageView = findViewById(R.id.imageView)
+        val upDiceImage: ImageView = findViewById(R.id.imageView2)
 
-        // Ustaw któe identyfikatory obrazu będą wyświetlone w zależności od rzutu kostką
-        val drawableResource = when (diceRoll) {
+        // Ustaw któe identyfikatory obrazu będą wyświetlone w zależności od rzutu kośćmi
+        val downDiceDrawableResource = when (downDiceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val upDiceDrawableResource = when (upDiceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -43,11 +54,13 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        // Zaktualizuj ImageView poprawnym identyfikatorem obazu kostki.
-        diceImage.setImageResource(drawableResource)
+        // Zaktualizuj ImageView poprawnym identyfikatorem obazu kości.
+        downDiceImage.setImageResource(downDiceDrawableResource)
+        upDiceImage.setImageResource(upDiceDrawableResource)
 
         // Zaktualizuj opis zawartości.
-        diceImage.contentDescription = diceRoll.toString()
+        downDiceImage.contentDescription = downDiceRoll.toString()
+        upDiceImage.contentDescription = upDiceRoll.toString()
     }
 }
 
