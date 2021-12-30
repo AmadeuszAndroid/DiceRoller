dc80afa28f15a28d2e44af7521e031e39d9eed20
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2021-12-30 16:09:39 +0100

    Wyświetlanie kostki z dwoma oczkami.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -26,14 +26,8 @@ class MainActivity : AppCompatActivity() {
         // Tworzenie dwóch kostek 6-ściennych i rzucenie nimi.
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val upDice = Dice(6)
-        val upDiceRoll = upDice.roll()
-
-        // Aktualizacja ekranu o rzut kostkami.
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-        val upTextView: TextView = findViewById(R.id.textView2)
-        upTextView.text = upDiceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
